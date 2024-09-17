4fa1e53b742937078978b963d6b88673d24f1626
skaerein <skaerein@gmail.com>
2024-09-17 22:09:24 +0700

    Vennycia - 2702251655

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from graphs import bfs, dfs, ucs, greedy_bfs, a_star
 
-# main.py
-
-# Declare the graph as an adjacency list with edge weights
 graph = {
-    <your code here>
+    'A': [('B', 1), ('C', 4)],
+    'B': [('A', 1), ('C', 2), ('D', 5)],
+    'C': [('A', 4), ('B', 2), ('D', 1), ('E', 5)],
+    'D': [('B', 5), ('C', 1), ('F', 2)],
+    'E': [('C', 3), ('F', 1)],
+    'F': [('D', 2), ('E', 1)],
+    'G': [('F', 3)]
 }
 
-# Declare the heuristic values for each node
 heuristics = {
-    <your code here>
+    'A': 7,
+    'B': 6,
+    'C': 4,
+    'D': 3,
+    'E': 2,
+    'F': 1,
+    'G': 0
 }
